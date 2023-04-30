db6c1d34916ce05459322240553b371651cc1a37
vanandre <vanoncini@hotmail.com>
2023-04-30 20:59:52 +0200

    Added client

---
+++ src/client.cpp
@@ -0,0 +1,20 @@
+#include "client.hpp"
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello from the client" << std::endl;
+    return 0;
+}
+
+Client::Client() {
+
+}
+
+Client::~Client() {
+
+}
+
+bool Client::write_result() {
+
+}
